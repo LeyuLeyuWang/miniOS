041457161d619dfa04188ce135ec8437cda5c78b
LeyuLeyuWang <146308149+LeyuLeyuWang@users.noreply.github.com>
2025-12-10 16:19:06 -0800

    Add interactive menu and enhance snake game

--- games/snake/snake.h
+++ games/snake/snake.h
@@ -2,8 +2,6 @@
 #define SNAKE_H
 
 #define INIT_LENGTH 3
-#define ROW         24
-#define COLUMN      80
 
 enum Direction
 {
@@ -17,24 +15,13 @@ typedef struct{
     int x, y;
 } Node;
 
-void init_scr();
 void init_snake();
-void move();
 void game_start();
-void draw();
-void copy();
-int overlap();
-void game_over();
-void game_restart();
-void keyboard();
-void generate_food();
-void eat();
 
 // keyboard constant
 #define UP          0x25   /* Up       */
 #define DOWN        0x26   /* Down     */
 #define LEFT        0x27   /* Left     */
 #define RIGHT       0x28   /* Right    */
-#define ENTER       0x03   /* Enter    */
 
 #endif
